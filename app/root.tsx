f9a7fa26a6f1d0d5f4ce5469792ea5aef8ac9856
Peter Bengtsson <mail@peterbe.com>
2025-01-26 10:34:17 -0500

    favicon stuff

--- app/root.tsx
+++ app/root.tsx
@@ -23,6 +23,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.manifest" />
         <Meta />
         <Links />
       </head>
