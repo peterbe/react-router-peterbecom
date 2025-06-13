b5f991e7e8e3073658ae1199cf22130ab03193f4
Peter Bengtsson <mail@peterbe.com>
2025-06-13 06:46:41 -0400

    requestLogger (#144)

---
+++ server/requestLogger.ts
@@ -0,0 +1,68 @@
+import { isbot } from "isbot"
+import onFinished from "on-finished"
+
+import type { NextFunction, Request, Response } from "express"
+
+export function requestLogger() {
+  return function logger(req: Request, res: Response, next: NextFunction) {
+    const _startAt = process.hrtime()
+
+    function logRequest() {
+      // time elapsed from request start
+      const elapsed = process.hrtime(_startAt)
+
+      const ms = elapsed[0] * 1e3 + elapsed[1] * 1e-6
+      const userAgent = req.headers["user-agent"] || ""
+
+      const isbot_ = isbot(userAgent)
+      let botUrl: null | string = null
+      if (isbot_ && userAgent) {
+        const match = userAgent.match(/\+(http.*)\)/)
+        if (match) {
+          botUrl = match[1]
+        }
+      }
+      const referer = req.headers.referer || null
+      const contentType = res.getHeader("content-type") ?? null
+      const contentLength = res.getHeader("content-length") ?? null
+      const contentEncoding = res.getHeader("content-encoding") ?? null
+      const data = {
+        request: {
+          method: req.method,
+          path: req.path,
+          query: req.query,
+          url: req.url,
+          userAgent,
+          referer,
+          forwardedHost: req.headers["x-forwarded-host"] ?? null,
+          host: req.headers.host ?? null,
+        },
+        response: {
+          statusCode: res.statusCode,
+          contentType,
+          contentLength,
+          contentEncoding,
+        },
+        meta: {
+          elapsedMs: ms,
+          isbot: isbot_,
+          botUrl,
+          contentLengthSize: contentLength ? Number(contentLength) : 0,
+        },
+      }
+      console.log(data)
+    }
+
+    function logRequestTried() {
+      try {
+        return logRequest()
+      } catch (error) {
+        console.error("Failure to log request:", error)
+      }
+    }
+
+    onFinished(res, logRequestTried)
+
+    next()
+  }
+}
