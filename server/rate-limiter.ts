ff76e13a035d1137a632cb10dcf2e7059b04b259
Peter Bengtsson <mail@peterbe.com>
2025-09-07 15:26:05 -0400

    rate limit certain bot agents (#242)
    * rate limit certain bot agents
    
    * add integration test

---
+++ server/rate-limiter.ts
@@ -0,0 +1,31 @@
+import type { Request } from "express"
+import { rateLimit } from "express-rate-limit"
+import { isbot } from "isbot"
+import { getBotAgent } from "./get-bot-agent"
+
+// Picking these based on https://admin.peterbe.com/analytics/charts#bot-agent-requests
+const RATE_LIMIT_BOT_AGENTS = new Set([
+  "https://developers.facebook.com/docs/sharing/webmasters/crawler",
+  "http://www.bing.com/bingbot.htm",
+  "http://www.semrush.com/bot.html",
+])
+
+export const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000,
+  limit: 25,
+  skip: (req: Request) => {
+    const userAgent = req.headers["user-agent"] || ""
+    if (userAgent) {
+      const isbot_ = isbot(userAgent)
+      if (isbot_ && userAgent) {
+        const botAgent = getBotAgent(userAgent) || userAgent
+        if (RATE_LIMIT_BOT_AGENTS.has(botAgent)) {
+          return false
+        }
+      }
+    }
+
+    // Default is to skip rate limiting entirely
+    return true
+  },
+})
