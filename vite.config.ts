acfdd82ec5890ed5e3d945f247bf336c321fb096
Peter Bengtsson <mail@peterbe.com>
2026-08-21 07:49:06 -0400

    bla

--- vite.config.ts
+++ vite.config.ts
@@ -8,37 +8,30 @@ const BACKEND_BASE_URL = process.env.API_BASE || "http://127.0.0.1:8888"
 const backendProxy = {
   target: BACKEND_BASE_URL,
 }
-export default defineConfig((config) => {
-  return {
-    css: {
-      postcss: {
-        plugins: [autoprefixer],
-      },
-      // From https://github.com/picocss/pico/issues/717#issuecomment-3695614717
-      preprocessorOptions: { scss: { quietDeps: true } },
+export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [autoprefixer],
     },
-    plugins: [dynamicImagesPlugin(), reactRouter()],
-    server: {
-      port: 3000,
-      proxy: {
-        "/events": {
-          target: BACKEND_BASE_URL,
-          rewrite: () => "/api/v1/events",
-        },
-        "/cache/": backendProxy,
-        "/api/": backendProxy,
-        "/avatar.random.png": backendProxy,
-        "/avatar.png": backendProxy,
+    // From https://github.com/picocss/pico/issues/717#issuecomment-3695614717
+    preprocessorOptions: { scss: { quietDeps: true } },
+  },
+  plugins: [dynamicImagesPlugin(), reactRouter()],
+  server: {
+    port: 3000,
+    proxy: {
+      "/events": {
+        target: BACKEND_BASE_URL,
+        rewrite: () => "/api/v1/events",
       },
+      "/cache/": backendProxy,
+      "/api/": backendProxy,
+      "/avatar.random.png": backendProxy,
+      "/avatar.png": backendProxy,
     },
-    // Hack from https://github.com/remix-run/react-router/issues/12568#issuecomment-2692406113
-    resolve:
-      config.command === "build"
-        ? {
-            alias: {
-              "react-dom/server": "react-dom/server.node",
-            },
-          }
-        : undefined,
-  }
+  },
+  // Hack from https://github.com/remix-run/react-router/issues/12568#issuecomment-2692406113
+  resolve: {
+    tsconfigPaths: true,
+  },
 })
